1d227340a3f0250fedda89a4fa4a546754892b16
ajengauuliaa <ajengauliaa7@gmail.com>
2025-11-30 19:27:27 +0700

    ajax-barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -71,4 +71,34 @@ class BarangController extends Controller
             return redirect()->route('barang.index')->with('error', 'Barang tidak bisa dihapus, mungkin sudah ada di transaksi.');
         }
     }
+
+    public function getData()
+    {
+        $barang = Barang::with(['kategori', 'pemasok'])
+            ->withSum('masuk', 'jumlah')
+            ->withSum('keluar', 'jumlah')
+            ->get();
+
+        return response()->json($barang);
+    }
+
+    public function storeAjax(Request $request)
+    {
+        $request->validate([
+            'barang' => 'required|string|max:45',
+            'deskripsi' => 'required|string',
+            'satuan' => 'required|string|max:10',
+            'id_kategori' => 'required|exists:kategori,id_kategori',
+            'id_pemasok' => 'required|exists:pemasok,id_pemasok',
+        ]);
+
+        $barang = Barang::create($request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Barang berhasil ditambahkan',
+            'data' => $barang,
+    ]);
+    }
+
 }
